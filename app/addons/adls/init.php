38959381d59f2bf6c482a874341c85e9cec1d122
WSergio@NZXT <sergiu@disruptive.academy>
2015-10-31 11:18:35 +0200

    Added structure

---
+++ app/addons/adls/init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * HELOstore
+ *
+ * This source file is part of a commercial software. Only users who have purchased a valid license through
+ * https://helostore.com/ and accepted to the terms of the License Agreement can install this product.
+ *
+ * @category   Add-ons
+ * @package    HELOstore
+ * @copyright  Copyright (c) 2015-2016 HELOstore. (https://helostore.com/)
+ * @license    https://helostore.com/legal/license-agreement/   License Agreement
+ * @version    $Id$
+ */
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
