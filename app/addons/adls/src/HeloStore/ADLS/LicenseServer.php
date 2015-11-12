393ca485f4d3a8e65e63783e85638f2a8dc41233
WSergio@NZXT <sergiu@disruptive.academy>
2015-11-12 02:34:32 +0200

    Added basic update mechanism

--- app/addons/adls/src/HeloStore/ADLS/LicenseServer.php
+++ app/addons/adls/src/HeloStore/ADLS/LicenseServer.php
@@ -263,7 +263,94 @@ class LicenseServer
 			return $response;
 		}
 		$customerProduct = $request['product'];
-		$path = DIR_ROOT . '/var/releases/';
+		$path = DIR_ROOT . '/var/releases/autoimage_lite-v0.1.2.zip';
+
+		@apache_setenv('no-gzip', 1);
+		@ini_set('zlib.output_compression', 'Off');
+		if (!is_file($path)) {
+			$response['code'] = LicenseClient::CODE_ERROR_UPDATE_INVALID_REMOTE_PATH;
+			return $response;
+		}
+		$size  = filesize($path);
+		$file = @fopen($path,"rb");
+		if (empty($file)) {
+			$response['code'] = LicenseClient::CODE_ERROR_UPDATE_FAILED_REMOTE_FILE_OPEN;
+			return $response;
+		}
+		$pathInfo = pathinfo($path);
+		$filename = $pathInfo['filename'] . '.' . $pathInfo['extension'];
+		$isAttachment = false;
+
+		// set the headers, prevent caching
+		header("Pragma: public");
+		header("Expires: -1");
+		header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+
+		// set appropriate headers for attachment or streamed file
+		if ($isAttachment) {
+			header("Content-Disposition: attachment; filename=\"$filename\"");
+		} else {
+			header('Content-Disposition: inline;');
+		}
+		header("Content-Type: application/zip");
+
+		//check if http_range is sent by browser (or download manager)
+		if (isset($_SERVER['HTTP_RANGE'])) {
+			list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+			if ($size_unit == 'bytes') {
+				//multiple ranges could be specified at the same time, but for simplicity only serve the first range
+				//http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+				list($range, $extra_ranges) = explode(',', $range_orig, 2);
+			} else {
+				$range = '';
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+				exit;
+			}
+		} else {
+			$range = '';
+		}
+
+		//figure out download piece from range (if set)
+		list($seek_start, $seek_end) = explode('-', $range, 2);
+
+		//set start and end based on range (if set), else set defaults
+		//also check for invalid ranges.
+		$seek_end   = (empty($seek_end)) ? ($size - 1) : min(abs(intval($seek_end)),($size - 1));
+		$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
+
+		//Only send partial content header if downloading a piece of the file (IE workaround)
+		if ($seek_start > 0 || $seek_end < ($size - 1))
+		{
+			header('HTTP/1.1 206 Partial Content');
+			header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$size);
+			header('Content-Length: '.($seek_end - $seek_start + 1));
+		}
+		else
+			header("Content-Length: $size");
+
+		header('Accept-Ranges: bytes');
+
+		set_time_limit(0);
+		fseek($file, $seek_start);
+
+		while(!feof($file))
+		{
+			print(@fread($file, 1024*8));
+			ob_flush();
+			flush();
+			if (connection_status()!=0)
+			{
+				@fclose($file);
+				exit;
+			}
+		}
+
+		// file save was a success
+		@fclose($file);
+
+		$response['code'] = LicenseClient::CODE_SUCCESS;
+
+		return $response;
 	}
 
 }
